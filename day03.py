4dfad07a32046fed4d55b4dc0e155b5b0f1af5fc
Jiri Zanat <jiri.zanat@promenszlin.com>
2025-12-03 09:25:45 +0100

    day3 complete

--- day03.py
+++ day03.py
@@ -2,7 +2,7 @@ from file import read_data
 
 test = read_data('03test.txt')
 actual = read_data('day03.txt')
-
+"""
 batteries = actual
 total = 0
 for bank in batteries:
@@ -16,10 +16,33 @@ for bank in batteries:
     total += cur_max
     
 print(total)
-    
-batteries = test
+"""    
+batteries = actual #['811384523523527']
 total = 0
 for bank in batteries:
-    seznam = list(bank)
-    print(seznam)
-    for i in range(3)
+    #print(len(bank), 'znaku')
+    output = ''
+    curr_i = 0
+    #bank += '0'
+    #print(bank)
+    for idx in range(12):
+        curr_num = '0'
+        end = len(bank[curr_i:])-(11-idx)
+        #print(curr_i,end)       
+        test_string = bank[curr_i:curr_i+end] 
+        #print(test_string)
+        #input()
+        for j,char in enumerate(test_string):
+            if int(char)> int(curr_num):
+                curr_num = char
+                max_j = j
+        output += curr_num
+        curr_i += max_j + 1
+        
+        #print(';'.join([output,test_string]))
+        #input()
+    total += int(output)
+
+print(total)
+
+
