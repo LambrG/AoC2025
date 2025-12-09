bd74c6c047067c3d35e1f6257329a7e2d52df0ce
Jiri Zanat <jiri.zanat@promenszlin.com>
2025-12-09 15:47:56 +0100

    Remove duplicate code

--- day09.py
+++ day09.py
@@ -8,31 +8,20 @@ def area_of_rectangle(row1, col1, row2, col2):
     return width * height
 
 
-def solve1(grid):
-    max_area = 0
-    for i in range(len(grid)):
-        row1, col1 = grid[i]
-        for j in range(i + 1, len(grid)):
-            row2, col2 = grid[j]
-            area = area_of_rectangle(row1, col1, row2, col2)
-            if area > max_area:
-                max_area = area 
-    return max_area    
-
-
-def solve2(grid):
-    max_area = 0
+def solve(grid):
+    max_area_1 = 0
+    max_area_2 = 0
     polygon = Polygon(grid)
     for i in range(len(grid)):
         row1, col1 = grid[i]
         for j in range(i + 1, len(grid)):
             row2, col2 = grid[j]
+            area = area_of_rectangle(row1, col1, row2, col2)
+            max_area_1 = max(max_area_1, area)
             rectangle = box(min(row1, row2), min(col1,col2), max(row1,row2), max(col1, col2))
             if polygon.contains(rectangle):
-                area = area_of_rectangle(row1, col1, row2, col2)
-                if area > max_area:
-                    max_area = area
-    return max_area
+                max_area_2 = max(max_area_2, area)
+    return max_area_1, max_area_2
 
 
 def main():
@@ -41,11 +30,14 @@ def main():
     actual = read_data('inputs/day09.txt')
     actual_grid = process_data_to_grid_points(actual)
 
-    assert solve1(test_grid) == 50, f"Expected 50 but got {solve1(test_grid)}"
-    print(solve1(actual_grid))
+    test_result1, test_result2 = solve(test_grid)
+    actual_result1, actual_result2 = solve(actual_grid)
 
-    assert solve2(test_grid) == 24, f"Expected 24 but got {solve2(test_grid)}"
-    print(solve2(actual_grid))
+    assert test_result1 == 50, f"Expected 50 but got {test_result1}"
+    print(actual_result1)
+
+    assert test_result2 == 24, f"Expected 24 but got {test_result2}"
+    print(actual_result2)
 
 
 if __name__ == "__main__":
